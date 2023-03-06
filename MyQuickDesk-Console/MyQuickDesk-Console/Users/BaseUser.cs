3dbf9c845883aac4b09f193b878b483ccd06bd4d
Mariusz Wilczek <m.wilczek1998@gmail.com>
2023-03-06 19:02:52 +0100

    Owner, Tenant and BaseUser added

--- MyQuickDesk-Console/MyQuickDesk-Console/Users/BaseUser.cs
+++ MyQuickDesk-Console/MyQuickDesk-Console/Users/BaseUser.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace MyQuickDesk_Console.Users
 {
-    internal class BaseUser
+    public class User
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public string role { get; set; }
+        public string email { get; set; }
+
+        public User(int id, string username, string firstname, string lastname, string password, string role, string email)
+        {
+            Id = id;
+            Username = username;
+            FirstName = firstname;
+            Lastname = lastname;
+            Password = password;
+            role = role;
+            email = email;
+        }
+
+
     }
 }
--- MyQuickDesk-Console/MyQuickDesk-Console/Users/Tenant.cs
+++ MyQuickDesk-Console/MyQuickDesk-Console/Users/Tenant.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace MyQuickDesk_Console.Users
 {
-    internal class Tenant
+    internal class Tenant : User
     {
+        public Tenant(int id, string username, string firstname, string lastname, string password, string role, string email) : base(id, username, firstname, lastname, password, role, email)
+        { }
+
+        public void AvaibleRooms(Logika_Beznesowa.TheRoom room)
+        {
+            //Sprawdź dostępne pokoje
+        }
+        public void MyReservations(Reservation reservation)
+        {
+            // Sprawdź moje rezerwacje
+        }
+        public void AddReservation(Reservation reservation)
+        {
+            // dodanie rezerwacji do listy rezerwacji
+        }
+
+        public void ModifyReservation(Reservation reservation)
+        {
+            // modyfikacja istniejącej rezerwacji
+        }
+
+        public void DeleteReservation(Reservation reservation)
+        {
+            // usunięcie rezerwacji z listy rezerwacji
+        }
     }
-}
