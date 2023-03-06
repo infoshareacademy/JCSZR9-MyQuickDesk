a4a188c0969c50e5f2d7108e2ea82c72f5e024e4
Mariusz Wilczek <m.wilczek1998@gmail.com>
2023-03-06 19:14:17 +0100

    Folder name users named

--- MyQuickDesk-Console/MyQuickDesk-Console/Users/BaseUser.cs
+++ MyQuickDesk-Console/MyQuickDesk-Console/Users/BaseUser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyQuickDesk_Console.Users
+namespace Users.Users
 {
     public class User
     {
--- MyQuickDesk-Console/MyQuickDesk-Console/Users/Tenant.cs
+++ MyQuickDesk-Console/MyQuickDesk-Console/Users/Tenant.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyQuickDesk_Console.Users
+namespace Users.Users
 {
     internal class Tenant : User
     {
