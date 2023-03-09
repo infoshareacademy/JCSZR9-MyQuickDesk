d62459f54f7144d802d611837d4543bbeb3739e3
AdrianCebulski2000 <adriancebulski@gmail.com>
2023-03-09 10:38:46 +0100

    Class LoadingFile updated

--- Loading/Loading/LoadingFile.cs
+++ Loading/Loading/LoadingFile.cs
@@ -2,9 +2,29 @@
 {
     public class LoadingFile
     {
-        static void Main(string[] args)
+        string filePath = "";
+        public static Room LoadingFromFile(string filePath)
         {
-            Console.WriteLine("cf"); 
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath)) 
+                {
+                    int id = int.Parse(reader.ReadLine());
+                    string name = reader.ReadLine();
+                    int ownerId = int.Parse(reader.ReadLine());
+                    bool interactiveBoard = bool.Parse(reader.ReadLine());
+                    int capacity = int.Parse(reader.ReadLine());
+                    string description = reader.ReadLine();
+                    decimal price = decimal.Parse(reader.ReadLine());
+                    return new Room (id, name, ownerId, capacity, interactiveBoard, capacity, description, price);
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Ładownaie pliku nie powiodło się" + ex.Message);
+                return null;
+            }
+            Room room = Room.LoadFromFile(filePath);
         }
     }
 }
