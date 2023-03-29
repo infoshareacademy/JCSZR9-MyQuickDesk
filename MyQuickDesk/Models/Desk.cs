7308a1a2ed727e33abdba16b6c3ec5f389a88679
AdrianCebulski2000 <adriancebulski@gmail.com>
2023-03-29 18:03:39 +0200

    Desk class added

---
+++ MyQuickDesk/Models/Desk.cs
@@ -0,0 +1,14 @@
+﻿namespace MyQuickDesk.Models
+{
+    public class Desk
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public bool InteractiveBoard { get; set; }
+        public int Capacity { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ReservationDate { get; set; }
+    }
+}
