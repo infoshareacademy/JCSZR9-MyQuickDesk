44fadea8a9f26b328db93f1f5a3728ae0b5b569f
Diana Kosel <dianakosel@gmail.com>
2023-05-11 12:18:28 +0200

    mvc changes(delet id in views desk,parking) added ReservationItem in models

--- MyQuickDesk/Models/ParkingSpot.cs
+++ MyQuickDesk/Models/ParkingSpot.cs
@@ -1,14 +1,11 @@
 ﻿namespace MyQuickDesk.Models
 {
-    public class ParkingSpot
+    public class ParkingSpot:ReservationItem
     {
-        public int Id { get; set; }
-        public int PlaceNumber { get; set; }
-        public char FloorName { get; set; }
+        public int ReservationId { get; set; }
         public bool AccesibleParkingSpot { get; set; }
         public bool Charger { get; set; }
         public bool IsAvaible { get; set; }
-        public DateTime? StartDate  { get; set; }
-        public DateTime? EndDate { get; set; }
+        
     }
 }
---
+++ MyQuickDesk/Models/ReservationItem.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyQuickDesk.Models
+{
+    public abstract class ReservationItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "Owner Id")]
+
+        public int OwnerId { get; set; }
+
+        [StringLength(200)]
+        [Required(ErrorMessage = "Please provide description.")]
+
+        public string Description { get; set; }
+
+        [Display(Name = "Start Date")]
+        [Required(ErrorMessage = "Please provide start date.")]
+
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        [Required(ErrorMessage = "Please provide end date.")]
+
+        public DateTime EndDate { get; set; }
+    }
+}
